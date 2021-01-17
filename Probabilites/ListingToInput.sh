1de39904f1127dee724915ddd923d2b8e573a5b2
jhebertaz <j.hebert.a.z@gmail.com>
2021-01-17 09:22:50 -0500

    Update

--- Probabilites/ListingToInput.sh
+++ Probabilites/ListingToInput.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-{ shopt -s nullglob; for file in Exercice/*; do echo "\\input{${file//'.tex'}}\newpage\setcounter{subsection}{0}"; done; } > inputs.tex
+{ shopt -s nullglob; for file in Exercice/Exemple/*; do echo "\\input{${file//'.tex'}}\newpage\setcounter{subsection}{0}"; done; } > inputs.tex
 
